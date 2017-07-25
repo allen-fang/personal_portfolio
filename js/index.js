edd8ece3d749f9abec94f4a6a124a4326514a12f
Allen Fang <allenfang08@gmail.com>
2017-07-25 15:26:52 -0700

    changes to landing page

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ $('a[href*="#"]')
   .click(function(event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      location.pathname.replace(/^\//, '') == '#about'
       && 
       location.hostname == this.hostname
     ) {
@@ -28,6 +28,54 @@ $('a[href*="#"]')
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+    else if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length && target[0].className == "about-section") {
+        // Only prevent default if animation is actually gonna happen
+        console.log(target[0].className);
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - $(window).height()/3 + hdr
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+      else if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        console.log(target[0].className);
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top - hdr
